4ee87d00fe42df6ee3f92c7676c9e2ac33bb34b9
Taypan <taypan@email.cz>
2011-10-27 22:48:12 +0200

    remove getModel

--- presenters/DefaultPresenter.php
+++ presenters/DefaultPresenter.php
@@ -9,17 +9,6 @@ use Venne\Application\UI;
  */
 class DefaultPresenter extends \Venne\Developer\Presenter\FrontPresenter {
 
-	public function getModel(){
-		if(isset($this->model)){
-			return $this->model;
-		} else {
-			$this->model = New EmptyModel();
-		}
-	}
-	
-	/**
-	 * @privilege read
-	 */
 
 
 }
